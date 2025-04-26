64ca22dd14ec46edc8d1b4d16a83f3d955f93a07
Grzegorz Gil <grzegorzfgil@gmail.com>
2025-04-26 15:52:07 +0200

    Add map visualization in chat

--- src/ask_your_neighbour/agent_specs/orchestrator_agent.py
+++ src/ask_your_neighbour/agent_specs/orchestrator_agent.py
@@ -6,4 +6,5 @@ After gathering the information, pass the data to the summarization agent.
 Tool usage:
 - use all available tools to gather information
 - ensure to verify the accuracy of the gathered data
+- to visualize the data, use data_visualizer.visualize_to_user tool with coordinates of the location
 """
